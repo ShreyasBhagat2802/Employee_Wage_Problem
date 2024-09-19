d3f78f11a32a10d2973d5f9a39d7869dc86d06eb
Shreyas <shreyasbhagat2802@gmail.com>
2024-09-19 20:47:30 +0530

    UC4 added

--- EWP_main.py
+++ EWP_main.py
@@ -23,3 +23,23 @@ part_time_hours = 8
 part_time_wage = wage_per_hour * part_time_hours
     
 print(part_time_hours)
+
+#UC4
+def employee_type():
+    employee_status = random.randint(0, 2)
+    
+    match employee_status:
+        case 2:
+            return "full-time"
+        case 1:
+            return "part-time"
+        case _:
+            return "absent"
+
+employee_state = employee_type()
+if employee_state == "full-time":
+    print("Employee is Full Time. Wage: ", {full_time_wage})
+elif employee_state == "part-time":
+    print("Employee is Part Time. Wage: ", {part_time_wage})
+else:
+    print("Employee is Absent.")
