261ca99fd9b56178a7786db010193d664133d39e
Shreyas <shreyasbhagat2802@gmail.com>
2024-09-19 20:40:21 +0530

    UC 2 Added

--- EWP_main.py
+++ EWP_main.py
@@ -1,3 +1,14 @@
+print("Welcome to Employee Wage Computation Program on Master Branch")
+
+#UC1
+import random 
+attendance = random.randint(0, 1)
+    
+if attendance == 1:
+    print("Employee is Present")
+else:
+    print("Employee is Absent")
+
 #UC2
 wage_per_hour = 20
 full_day_hours = 8
