3bce4cad64ab3ebb2ef79aa48deb08299d1e6c1c
Shreyas <shreyasbhagat2802@gmail.com>
2024-09-19 16:02:16 +0530

    UC 1 Added

--- EWP_main.py
+++ EWP_main.py
@@ -1,1 +1,10 @@
 print("Welcome to Employee Wage Computation Program on Master Branch")
+
+#UC1
+import random 
+attendance = random.randint(0, 1)
+    
+if attendance == 1:
+    print("Employee is Present")
+else:
+    print("Employee is Absent")
